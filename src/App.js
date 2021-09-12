53acc0ec5d7328f3346427bf66d0c9790bbe0db7
vivek <rohanmourya879@gmail.com>
2021-09-12 21:59:20 +0530

    vivek commit

--- src/App.js
+++ src/App.js
@@ -8,15 +8,17 @@ import {v4 as uuidv4} from 'uuid';
 function App() {
   const history=useHistory();
   const video=useRef();
-  useEffect(()=>{
-    navigator.mediaDevices.getUserMedia({video:true,audio:true}).then((stream)=>{
-      video.current.srcObject=stream;
-      
-    })
+  
+
+
+  useEffect(()=>
+  {
+    navigator.mediaDevices.getUserMedia({video:true}).then((stream)=>video.current.srcObject=stream)
   },[])
   return (
     <div className="App">
-      <video style={{width:"500px" ,height:"500px"}} muted autoPlay ref={video}/>
+      {video&&<video style={{width:"500px" ,height:"500px"}} muted autoPlay ref={video}/>}
+      Rajuisldjginsoigsoidgjosdigosdiggsdoiigjsdoigjsdsdlgkksdgooisgoij
      
     </div>
   );
