4b5d18473806223d099cc0ed1493a7592d090325
Rahul <mouryarahul223@gmail.com>
2021-10-26 18:17:40 +0530

    debugging

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,7 +5,7 @@ import 'firebase/auth';
 console.log(process.env);
 
 const app=initializeApp({
-    apiKey: process.env.REACT_APP_API_KEY,
+    apiKey: process.env.REACT_APP_API_KEY.replace(/\\n/g, '\n'),
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
