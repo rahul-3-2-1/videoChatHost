ea79a284bba08c8200641dd312abc35c1bec1810
Rahul <mouryarahul223@gmail.com>
2021-10-26 14:17:08 +0530

    error

--- app.js
+++ app.js
@@ -173,8 +173,9 @@ io.on("connection", (socket) => {
 if(process.env.NODE_ENV === "production")
 {
   app.use(express.static("client/build"));
+  const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,  "client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname,  "client","build","index.html"));
   });
 }
 
